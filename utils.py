4d80f07b131ea107783d421685634a4bced640e4
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-12-08 19:10:41 +0330

    add otpgenerator to create otp with pyotp and send it to user

--- utils.py
+++ utils.py
@@ -1,12 +1,31 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 import requests
+import pyotp
+from django.utils import timezone
+from accounts.models import OtpCode
+from datetime import timedelta
 
-def send_otp_code(phone_number, code, action):
 
-	data = {'bodyId': 524, 'to': phone_number, 'args':[f'{code}']}
-	response = requests.post('your api key ', json=data)
-	print(response.json())
-#	print(f"Sending OTP code {code} to {phone_number} - {action}")
+class OtpGenerator:
+	@staticmethod
+	def generate_otp():
+		totp = pyotp.TOTP(pyotp.random_base32())
+		code = totp.now()
+		return code
+
+	@staticmethod
+	def send_otp(phone_number, code, action='default'):
+#		data = {'bodyId': 524, 'to': phone_number, 'args':[f'{code}']}
+#		response = requests.post('your api key ', json=data)
+#		print(response.json())
+		print(f"Sending OTP code {code} to {phone_number} - {action}")
+		OtpCode.objects.create(phone_number=phone_number, code=code, action=action)
+
+	@staticmethod
+	def cleanup_expired_code():
+		expiration_time = timezone.now() - timedelta(minutes=1)
+		OtpCode.objects.filter(created__lt=expiration_time).delete()
+
 
 class IsAdminUserMixin(UserPassesTestMixin):
 	def test_func(self):
